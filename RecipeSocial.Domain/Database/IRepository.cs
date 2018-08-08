3430a33875c2e028ae2f885d8260b8e14c2d8489
Ricardo Bueno <ricardo.bueno@bit2bitamericas.com>
2018-08-08 10:53:17 -0500

    Home and Repository

---
+++ RecipeSocial.Domain/Database/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using RecipeSocial.Domain.Entities.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace RecipeSocial.Domain.Database
+{
+    public interface IRepository<T> where T : Base
+    {
+        Task<T> GetAsync(long id);
+        Task<ICollection<T>> GetAllAsync();
+        Task<ICollection<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
