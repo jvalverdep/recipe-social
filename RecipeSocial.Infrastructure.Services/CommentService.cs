670ff19de17fe191a41115b86716a94612c9ad24
Ricardo Bueno <ricardo.bueno@bit2bitamericas.com>
2018-08-09 21:05:15 -0500

    Comments

---
+++ RecipeSocial.Infrastructure.Services/CommentService.cs
@@ -0,0 +1,27 @@
+﻿using RecipeSocial.Domain.Database;
+using RecipeSocial.Domain.Entities;
+using RecipeSocial.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeSocial.Infrastructure.Services
+{
+    public class CommentService : ICommentService
+    {
+        IRepository<Comment> repository;
+        public CommentService(IRepository<Comment> repository)
+        {
+            this.repository = repository;
+        }
+        public void Comment(int id, string text, int userId)
+        {
+            Comment comment = new Comment
+            {
+                RecipeId = id,
+                Text = text,
+                UserId = userId
+            };
+        }
+    }
+}
--- RecipeSocial.Interface.Web/Controllers/RecipesController.cs
+++ RecipeSocial.Interface.Web/Controllers/RecipesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RecipeSocial.Infrastructure.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,16 @@ namespace RecipeSocial.Interface.Web.Controllers
 {
     public class RecipesController
     {
+        private CommentService commentService;
+
+        public RecipesController(CommentService commentService)
+        {
+            this.commentService = commentService;
+        }
+
+        public void Comment(int id, string text)
+        {
+            commentService.Comment(id, text, 1);
+        }
     }
 }
